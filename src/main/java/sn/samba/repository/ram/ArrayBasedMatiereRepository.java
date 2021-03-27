6bdb3d8a7810531f688febaca74d863983d06696
Samband14 <samband456@gmail.com>
2021-03-27 09:52:42 +0000

    Update ArrayBasedMatiereRepository.java

--- src/main/java/sn/samba/repository/ram/ArrayBasedMatiereRepository.java
+++ src/main/java/sn/samba/repository/ram/ArrayBasedMatiereRepository.java
@@ -8,7 +8,7 @@ import sn.samba.repository.MatiereRepository;
 public class ArrayBasedMatiereRepository implements MatiereRepository {
 
         public static final Matiere[] MATIERES = {
-                new Matiere(1, "LC",),
+                new Matiere(1, "LC","Maths",),
                 new Matiere(2, "Python",),
                 new Matiere(3, "PHP",),
                 new Matiere(4, "JAVA",),
